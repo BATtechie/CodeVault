7cbca1823358dfa071d9aa98ea385bc0adb1ef54
BATtechie <yuvrajchandravansi2020@gmail.com>
2025-11-16 23:18:29 +0530

    added redirect

--- frontend/codeVault/src/App.jsx
+++ frontend/codeVault/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from './pages/SignIn';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Navigate to="/sign-in" replace />} />
         <Route path="/about" element={<div>About</div>} />
         <Route path="/sign-in" element={<SignIn/>} />
       </Routes>
